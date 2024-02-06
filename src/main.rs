fbcd0e6f99fbb93d9d34761e415c8effdcae5103
Clayton Fernalo <clafernalo@gmail.com>
2024-02-06 08:56:51 +0000

    fix: change ipv6 to ipv4

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs};
 use dotenv::dotenv;
 use server_inner::ServerInner;
 use tonic::transport::Server;
+use log::info;
 
 pub mod service {
     tonic::include_proto!("transcoder");
@@ -19,10 +20,13 @@ async fn main() -> Result<(), BoxError> {
     env_logger::init();
 
     let server = ServerInner::new();
+    let addr = "0.0.0.0:50051";
+
+    info!("Listening to {}", addr);
 
     Server::builder()
         .add_service(service::transcoder_server::TranscoderServer::new(server))
-        .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
-        .await?;
+        .serve(addr.to_socket_addrs().unwrap().next().unwrap())
+        .await?;    
     Ok(())
 }
