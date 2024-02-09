191017f0a7c1f474159dbdc59c8bd06708940c8e
Clayton Fernalo <clafernalo@gmail.com>
2024-02-09 20:53:32 +0000

    feat: add env support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use std::net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs};
+use std::{
+    env,
+    net::{Ipv4Addr, SocketAddrV4, ToSocketAddrs},
+};
 
 use dotenv::dotenv;
+use log::{error, info};
 use server_inner::ServerInner;
+use tokio::{
+    fs::create_dir_all,
+    process::Command,
+    time::{sleep, Duration},
+};
 use tonic::transport::Server;
-use log::info;
 
 pub mod service {
     tonic::include_proto!("transcoder");
@@ -24,9 +32,33 @@ async fn main() -> Result<(), BoxError> {
 
     info!("Listening to {}", addr);
 
+    // Run AWS-CLI updater
+    create_dir_all("./tmp/out").await?;
+    let mut cmd = Command::new("aws");
+    let aws_s3_output_uri = env::var("AWS_S3_OUTPUT_URI")?;
+    cmd.args([
+        "s3",
+        "sync",
+        "./tmp/out",
+        &aws_s3_output_uri,
+        "--exclude",
+        "*.tmp",
+        "--delete",
+    ]);
+    tokio::spawn(async move {
+        let mut cmd = cmd;
+        loop {
+            if cmd.status().await.is_err() {
+                error!("AWS S3 Sync failed");
+                break;
+            }
+            sleep(Duration::from_millis(500)).await;
+        }
+    });
+
     Server::builder()
         .add_service(service::transcoder_server::TranscoderServer::new(server))
         .serve(addr.to_socket_addrs().unwrap().next().unwrap())
-        .await?;    
+        .await?;
     Ok(())
 }
